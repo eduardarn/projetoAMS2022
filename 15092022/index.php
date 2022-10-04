e40f38796463d61c4bbf52a44f55281eeae63f8c
e d u a r d a <86473064+eduardarn@users.noreply.github.com>
2022-10-04 12:12:28 -0300

    Add files via upload

---
+++ 15092022/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <body>
+        <form action="media.php" method="post">
+            Insira a nota 1: <input type="text" name="nota1"><br>
+            Insira a nota 2: <input type="text" name="nota2"><br>
+            Insira a nota 3: <input type="text" name="nota3"><br>
+            <input type="submit">
+
+        </form>
+    </body>
+</html>
---
+++ 15092022/media.php
@@ -0,0 +1,8 @@
+<?php 
+    $nota1 = $_POST['nota1'];
+    $nota2 = $_POST['nota2'];
+    $nota3 = $_POST['nota3'];
+
+    $media = ($nota1 + $nota2 + $nota3)/3;
+    echo "A média final é: $media";
+?>
